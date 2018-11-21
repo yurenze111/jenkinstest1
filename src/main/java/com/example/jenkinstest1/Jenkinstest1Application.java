8c513085bb91622acd3efe28287721bc4a11088d
yurenze111 <3373633955@qq.com>
2018-11-21 15:52:58 +0800

    删除后上传

---
+++ src/main/java/com/example/jenkinstest1/Jenkinstest1Application.java
@@ -0,0 +1,15 @@
+package com.example.jenkinstest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jenkinstest1Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Jenkinstest1Application.class, args);
+        System.out.println("执行成功");
+    }
+
+}
---
+++ src/main/java/com/example/jenkinstest1/test1.java
@@ -0,0 +1,18 @@
+package com.example.jenkinstest1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author ze
+ * @date 2018/11/18 0018
+ */
+@RestController
+@SpringBootApplication
+public class test1 {
+    @RequestMapping(value = "/hello")
+    public String getting(){
+        return "hello world nihao";
+    }
+}
